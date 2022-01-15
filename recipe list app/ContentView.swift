46b8dd5fca81a4b3dedded53b1812ae481f56a0d
Anupama <anupama80286@gmail.com>
2022-01-15 17:06:23 +0530

    Initial Commit

---
+++ recipe list app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe list app
+//
+//  Created by Anupama Biswas on 15/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipe list app/recipe_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_list_appApp.swift
+//  recipe list app
+//
+//  Created by Anupama Biswas on 15/01/22.
+//
+
+import SwiftUI
+
+@main
+struct recipe_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
